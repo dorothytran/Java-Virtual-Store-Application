92c4f951520e8f924ce330f0f20de8d875cc5e79
Dorothy Tran <62575445+dorothytran@users.noreply.github.com>
2021-03-21 01:15:49 -0400

    Add files via upload

---
+++ StoreManager.java
@@ -0,0 +1,48 @@
+// Written by: Dorothy Tran 101141902 //
+// Partner: Cassidy Pacada 101143345 //
+
+public class StoreManager {
+
+    private Inventory inventory;
+
+    public StoreManager() {
+        inventory = new Inventory();
+    }
+
+    public StoreManager(Inventory storeInventory) {
+        inventory = storeInventory;
+    }
+
+    /** Checks the quantity of stock in store inventory */
+    public int checkStock(int productID) {
+        return inventory.getStock(productID);
+    }
+
+    public double transaction(int[][] cart) {
+
+        double totalCost = 0;
+
+        /** Checks inventory stock quantity */
+        for(int i = 0; i < cart.length; i++) {
+
+            int productID, quantity;
+            productID = cart[i][0];
+            quantity = cart[i][1];
+
+            /** If transaction is unsuccessful */
+            if (checkStock(productID) < quantity) {
+                return -1;
+            }
+        }
+        /** If transaction is successful */
+        for(int i = 0; i < cart.length; i++) {
+
+            int productID, quantity;
+            productID = cart[i][0]; // places product ID in first array
+            quantity = cart[i][1]; // places the quantity being purchased in second array
+            totalCost = (inventory.getPrice(productID) * quantity);
+            inventory.removeStock(quantity, productID);
+        }
+        return totalCost;
+    }
+}
